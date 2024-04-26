c3582deb9f27ffabab9d61726ff9d344cea44455
thkim610 <thkim610@naver.com>
2024-04-26 21:47:24 +0900

    mod: 커스텀 에러코드 정의 수정

--- src/main/java/com/example/user/common/exception/UserErrorCode.java
+++ src/main/java/com/example/user/common/exception/UserErrorCode.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 /**
- * User의 경우 800번대 에러코드 사용
+ * 커스텀 에러코드 사용
+ * U - User 관련 에러 코드
+ * R - 요청(request) 관련 에러코드
+ * S - 서버 내부 오류 관련 에러코드
  */
 
 @AllArgsConstructor
@@ -13,11 +16,11 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode {
 
     OK(HttpStatus.OK, "200", "성공"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "400", "잘못된 요청"),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "804", "회원 정보를 찾을 수 없습니다."),
-    USER_DUPLICATED(HttpStatus.BAD_REQUEST, "805", "중복된 회원 ID"),
-    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버 내부 오류"),
-    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR, "512", "Null point error")
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "R400", "잘못된 요청"),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "U404", "회원 정보를 찾을 수 없습니다."),
+    USER_DUPLICATED(HttpStatus.BAD_REQUEST, "U405", "중복된 회원 ID"),
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S500", "서버 내부 오류"),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR, "R512", "Null point error")
     ;
 
     private final HttpStatus httpStatus; //Http 상태코드
